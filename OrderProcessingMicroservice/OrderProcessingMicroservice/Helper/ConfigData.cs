214eac1b53e9b1f71b18ea8dc2719a776533d7ca
Yashwant Garad <P10480060@capita.co.uk>
2020-08-22 14:48:34 +0530

    Added send email helper

---
+++ OrderProcessingMicroservice/OrderProcessingMicroservice/Helper/ConfigData.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderProcessingMicroservice.Helper
+{
+    public static class ConfigData
+    {
+        private static IConfiguration _Configuration;
+
+        /// <summary>
+        /// This is static Configuration object which is implemented with Singleton pattern
+        /// </summary>
+        public static IConfiguration Configuration
+        {
+            get
+            {
+                if (_Configuration == null)
+                {
+                    _Configuration = (IConfiguration)new ConfigurationBuilder().AddJsonFile("sharedappsettings.json").Build();
+                }
+
+                return _Configuration;
+            }
+        }
+
+
+    }
+}
---
+++ OrderProcessingMicroservice/OrderProcessingMicroservice/Helper/SendMailHelper.cs
@@ -0,0 +1,50 @@
+﻿using OrderProcessingMicroservice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace OrderProcessingMicroservice.Helper
+{
+    public class SendMailHelper
+    {
+        /// <summary>
+        /// Send Mail to User.
+        /// </summary>
+        /// <param name="mailTemplate"></param>
+        /// <returns></returns>
+        public static Task<bool> SendMail(Email email)
+        {
+            try
+            {
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress(ConfigData.Configuration["SEND_GRID_FROMUSER"]),
+                    Subject = email.Subject,
+                    Body = email.HtmlContent,
+                };
+                //mailMessage.To.Add( email.Tos);
+
+                foreach (var item in email.Tos)
+                {
+                    mailMessage.To.Add(item.Email);
+                }
+
+                var smtpClient = new SmtpClient
+                {
+                    Credentials = new NetworkCredential(ConfigData.Configuration["SEND_GRID_USERNAME"], ConfigData.Configuration["SEND_GRID_PASSWORD"]),
+                    Host = "smtp.sendgrid.net",
+                    Port = 587
+                };
+                smtpClient.SendMailAsync(mailMessage);
+                return Task.FromResult(true);
+            }
+            catch (Exception)
+            {
+                return Task.FromResult(false);
+            }
+        }
+    }
+}
