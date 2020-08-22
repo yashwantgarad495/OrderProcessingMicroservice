7078a487b0a41c85a70e95ac1989327388b23aed
Yashwant Garad <P10480060@capita.co.uk>
2020-08-22 13:48:36 +0530

    setup for  folder structure and classes

---
+++ OrderProcessingMicroservice/OrderProcessingMicroservice/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderProcessingMicroservice.Models
+{
+    public class Order
+    {
+    }
+}
