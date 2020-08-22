76c8e43b1abfc5bb86b6e69519e0133d9bb8e832
Yashwant Garad <P10480060@capita.co.uk>
2020-08-22 14:13:56 +0530

    Added DataAccees layer

--- OrderProcessingMicroservice/OrderProcessingMicroservice/DataAccess/DataAccessOrder.cs
+++ OrderProcessingMicroservice/OrderProcessingMicroservice/DataAccess/DataAccessOrder.cs
@@ -1,11 +1,86 @@
-﻿using System;
+﻿using OrderProcessingMicroservice.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OrderProcessingMicroservice.DataAccess
 {
-    public class DataAccessOrder
+    public interface IDataAccessOrder
     {
+        Task<Order> GetOrderByID(int id);
+        Task<List<Order>> GetAllOrders();
+        Task<bool> Add(Order orderRequest);
+        Task<bool> Update(Order orderRequest);
+        Task<bool> Delete(int orderId);
+
+    }
+    public class DataAccessOrder : IDataAccessOrder
+    {
+        //TOOD: create object for Database connection string
+
+        /// <summary>
+        /// Get order by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<Order> GetOrderByID(int id)
+        {
+
+            //TODO : Filter record by id and return 
+
+            Order orderDetials = new Order();
+
+            return orderDetials;
+        }
+
+        /// <summary>
+        /// Get all orders
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<Order>> GetAllOrders()
+        {
+
+            //TODO : returns all orders from backend 
+
+            List<Order> ordersDetials = new List<Order>();
+
+            return ordersDetials;
+        }
+        /// <summary>
+        /// save order after processed 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<bool> Add(Order orderReuqest)
+        {
+
+            //TODO : save order into database after processed. 
+
+            return true;
+        }
+
+        /// <summary>
+        /// Update order after processed 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<bool> Update(Order orderReuqest)
+        {
+
+            //TODO : update order into database after changes. 
+
+            return true;
+        }
+        /// <summary>
+        /// Delete specific order by id 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<bool> Delete(int orderId)
+        {
+
+            //TODO : Delete order into database after changes. 
+
+            return true;
+        }
+
     }
 }
