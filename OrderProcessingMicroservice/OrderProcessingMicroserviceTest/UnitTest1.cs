a74fc39253b07d51a3f19b205edd76af9f4db847
Yashwant Garad <P10480060@capita.co.uk>
2020-08-22 13:41:54 +0530

    Initial project setup

---
+++ OrderProcessingMicroservice/OrderProcessingMicroserviceTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OrderProcessingMicroserviceTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
