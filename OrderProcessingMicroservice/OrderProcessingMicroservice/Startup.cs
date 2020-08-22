ff4574ca938a6f19cb0bb06ac9c7abb1f8f743b1
Yashwant Garad <P10480060@capita.co.uk>
2020-08-22 15:34:24 +0530

    Integreate swagger UI

--- OrderProcessingMicroservice/OrderProcessingMicroservice/Startup.cs
+++ OrderProcessingMicroservice/OrderProcessingMicroservice/Startup.cs
@@ -31,6 +31,8 @@ namespace OrderProcessingMicroservice
 
             services.AddTransient<IProcessOrderRepository, ProcessOrderRepository>();
             services.AddTransient<IDataAccessOrder, DataAccessOrder>();
+            // Inject an implementation of ISwaggerProvider with defaulted settings applied
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +50,15 @@ namespace OrderProcessingMicroservice
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Order Processing Microservice");
+            });
         }
     }
 }
